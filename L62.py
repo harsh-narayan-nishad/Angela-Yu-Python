fd9330fea99e15f099a7a0d784919501c9607038
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-04-10 13:05:29 +0530

    Day-10

---
+++ L62.py
@@ -0,0 +1,7 @@
+def greet(name, location):
+    print(f"Hello {name}")
+    print(f"I'm {name} and I'm from {location}")
+
+
+greet(name= "Harsh", location="Greater Noida")
+greet(name="Rohan", location = "Delhi")
